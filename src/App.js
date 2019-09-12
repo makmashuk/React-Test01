c06fc658eaa88f95eb558356475afd288b90da1e
makmashuk <mohiuddin@eatlbd.com>
2019-09-12 15:57:10 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   
+  render(){
+    return (
+      <div className="App">
+         <Clock />
+         <Toggle />
+         <List numbers={[11,12,15,"adasd",13]} />
+      </div>
+    );
+  }
+  
+}
+
+
+
+
+
+
+
+
+class ListItem extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      item:props.item
+    };
+  }
+  render(){
+    return(
+      <li> {this.state.item}</li>
+    )
+  }
+}
+class List extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      numbers: props.numbers
+    };
+    this.list =this.state.numbers.map(
+      (number)=>
+      <ListItem key={number.toString()} item={number}/> 
+    )
+  }
+
+ 
+  render(){
+    return(
+     <ul>
+       {this.list}
+     </ul>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+class Toggle extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={isToggleOn:false};
+
+    this.handleToogle=this.handleToogle.bind(this);
+  }
+  handleToogle(data){
+    this.setState(state=>({
+      isToggleOn: !state.isToggleOn
+    }));
+    console.log(data);
+  }
+  render(){
+    return(
+      <div className="toggleBtn">
+        <button onClick={this.handleToogle.bind(this,{name:"mak"})} >
+        { this.state.isToggleOn? "Toogle On" : "Toggle Off" }
+        </button>
+      </div>
+    );
+  }
+}
+
+
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date() }
+  }
+  componentDidMount(){
+    this.timer=setInterval(() => {
+      this.updateTime()
+    }, 1000);
+  }
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+
+  updateTime(){
+    this.setState({date: new Date()})
+  }
+  render(){
+    return (
+        <div>
+          { this.state.date.toLocaleTimeString() }
+        </div>
+         
+    );
+  }
 }
 
 export default App;
